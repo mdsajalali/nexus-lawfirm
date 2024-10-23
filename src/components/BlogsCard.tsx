749fe628ac4de927e2224ab8cd81e9ef6b5821c2
mdsajalali <mdsajalali2020@gmail.com>
2024-10-23 12:52:41 +0600

    add: blogs

---
+++ src/components/BlogsCard.tsx
@@ -0,0 +1,34 @@
+interface Blog {
+  img: string;
+  category: string;
+  title: string;
+  desc: string;
+  user: string;
+  user_name: string;
+  date: string;
+}
+
+interface BlogsCardProps {
+  blog: Blog;
+}
+
+const BlogsCard = ({ blog }: BlogsCardProps) => {
+  return (
+    <div className="border rounded-lg  p-1">
+      <img src={blog.img} alt="Blog" className="w-full h-auto" />
+      <h3 className="pt-4 pb-2 text-sm md:text-base">{blog.category}</h3>
+      <h1 className="text-lg md:text-xl cursor-pointer font-semibold hover:text-secondary transition-all">
+        {blog.title}
+      </h1>
+      <p className="py-2 text-base leading-6 line-clamp-2">{blog.desc}</p>
+      <div className="flex items-center gap-2 pt-2">
+        <img src={blog.user} alt="User" className="w-6 object-cover h-6 rounded-full" />
+        <div className="text-xs md:text-sm">
+          <span>{blog.user_name}</span> / <span>{blog.date}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogsCard;
--- src/pages/Blogs/Blogs.tsx
+++ src/pages/Blogs/Blogs.tsx
@@ -1,7 +1,29 @@
 import SharedNavbar from "../../shared/SharedNavbar";
-import banner from "../../images/blogs/blogs_banner.png"
+import banner from "../../images/blogs/blogs_banner.png";
+import { useEffect, useState } from "react";
+import BlogsCard from "../../components/BlogsCard";
+import Container from "../../shared/Container";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await fetch("/blogs.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setBlogs(data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
   return (
     <>
       <SharedNavbar
@@ -10,6 +32,23 @@ const Blogs = () => {
         bread_text="Blogs"
         bread_link="blogs"
       />
+      <Container>
+        <div className="grid grid-cols-12 gap-6 py-20 font-opensans lg:py-[100px]">
+          <div className="col-span-8">
+            <h1 className="text-2xl font-semibold md:text-[40px]">Our Blogs</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 py-[50px]">
+              {blogs.length > 0 ? (
+                blogs.slice(0, 6).map((blog, idx) => <BlogsCard key={idx} blog={blog} />)
+              ) : (
+                <p>No blogs available.</p>
+              )}
+            </div>
+          </div>
+          <div className="col-span-4">
+            <h1>Another Content</h1>
+          </div>
+        </div>
+      </Container>
     </>
   );
 };
