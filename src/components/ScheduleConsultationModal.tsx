f6becda5cdfee1b907305aef4087837c3db9730c
mdsajalali <mdsajalali2020@gmail.com>
2024-10-29 14:44:52 +0600

    add: modal animation

--- src/components/ScheduleConsultationModal.tsx
+++ src/components/ScheduleConsultationModal.tsx
@@ -1,5 +1,6 @@
 import { MouseEvent, useEffect } from "react";
 import { IoMdClose } from "react-icons/io";
+import {motion} from "framer-motion"
 
 interface ScheduleConsultationModalProps {
   isOpen: boolean;
@@ -43,11 +44,23 @@ const ScheduleConsultationModal = ({
         >
           <IoMdClose />
         </button>
-        <h1 className="pb-5 text-2xl font-semibold md:pb-10 md:text-[40px]">
+        <motion.h1
+          initial={{ scale: 0.9, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true }}
+          className="pb-5 text-2xl font-semibold md:pb-10 md:text-[40px]"
+        >
           Get in Touch with Us
-        </h1>
+        </motion.h1>
         <form>
-          <div className="mb-4 flex flex-col gap-[10px] md:flex-row md:gap-4">
+          <motion.div
+            initial={{ scale: 0.9, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            viewport={{ once: true }}
+            className="mb-4 flex flex-col gap-[10px] md:flex-row md:gap-4"
+          >
             <input
               type="text"
               placeholder="Enter your full name"
@@ -60,30 +73,46 @@ const ScheduleConsultationModal = ({
               className="mt-1 w-full rounded border p-3 font-opensans text-[12px] text-black outline-none md:mt-0 md:text-[14px]"
               required
             />
-          </div>
+          </motion.div>
 
-          <div className="mb-4 flex flex-col">
+          <motion.div
+            initial={{ scale: 0.9, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+            className="mb-4 flex flex-col"
+          >
             <input
               type="text"
               placeholder="Enter the subject of your message"
               className="rounded border p-3 font-opensans text-[12px] text-black outline-none md:text-[14px]"
               required
             />
-          </div>
-          <div className="mb-4 flex flex-col">
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0.9, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 1.2 }}
+            viewport={{ once: true }}
+            className="mb-4 flex flex-col"
+          >
             <textarea
               placeholder="Type your message here"
               className="rounded border p-3 font-opensans text-[12px] text-black outline-none md:text-[14px]"
               required
               rows={4}
             ></textarea>
-          </div>
-          <button
+          </motion.div>
+          <motion.button
+            initial={{ scale: 0.9, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 1.4 }}
+            viewport={{ once: true }}
             type="submit"
             className="rounded border bg-white px-16 py-[14px] font-opensans text-[14px] font-semibold text-black transition-all hover:bg-primary hover:text-white md:px-7 md:text-[18px]"
           >
             Request consultation
-          </button>
+          </motion.button>
         </form>
       </div>
     </div>
